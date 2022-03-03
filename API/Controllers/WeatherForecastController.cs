52c1195d108cdbaf777d57bf78b213f54c0aac4b
FrankBatista1 <fb53901@gmail.com>
2022-03-02 22:16:11 -0500

    .net 5 in .net 6 for learning purpouses

--- API/Controllers/WeatherForecastController.cs
+++ API/Controllers/WeatherForecastController.cs
@@ -1,7 +1,11 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
 
-namespace API.Controllers;
-
+namespace API.Controllers
+{
 [ApiController]
 [Route("[controller]")]
 public class WeatherForecastController : ControllerBase
@@ -30,3 +34,6 @@ public class WeatherForecastController : ControllerBase
         .ToArray();
     }
 }
+}
+
+
